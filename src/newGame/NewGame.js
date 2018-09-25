af845d964571d129b03341e935ef5138efcc6432
sjthn <srijithn@thoughtworks.com>
2018-09-25 15:07:06 +0530

    don't show scorer in new game screen

--- src/newGame/NewGame.js
+++ src/newGame/NewGame.js
@@ -4,7 +4,6 @@ import Scorer from '../scorer/Scorer';
 const NewGame = () => (
   <div>
     <h2>This is the page where the New Game Details have to be entered</h2>
-    <Scorer />
   </div>
 );
 export default NewGame;
