6ff49db3b0269de8418747df1d2b51196b8d7def
Puneet <in-puneet.patwari@in-puneetpatwari.local>
2018-09-27 10:16:23 +0530

    Refactored BallScore file for better readability {Puneet}

--- src/scorer/BallScore.jsx
+++ src/scorer/BallScore.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Col, Button, Row } from 'reactstrap';
+import { Container, Col, Button, Row, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { connect } from 'react-redux';
 import { updateOverDetails, updateScoreDetails, swapBatsman } from './actions';
 import './Scorer.css';
@@ -10,7 +10,10 @@ class BallScore extends Component {
     this.state = {
       run: -1,
       extra: '',
+      isOut: false,
+      modal: true,
     };
+    this.toggleModal = this.toggleModal.bind(this);
   }
   onNextBallClick(run, extra) {
     this.props.handleCurrentBall(run, extra);
@@ -21,7 +24,7 @@ class BallScore extends Component {
     this.setState({ run: Number(e.target.value) });
   }
   setExtra(e) {
-    this.setState({ extra: e.target.value });
+    this.setState({ extra: this.state.extra === e.target.value ? '' : e.target.value });
   }
   resetExtra() {
     this.setState({ extra: '' });
@@ -29,7 +32,13 @@ class BallScore extends Component {
   resetRun() {
     this.setState({ run: -1 });
   }
-  render() {
+  toggleIsOut() {
+    this.setState({ isOut: !this.state.isOut });
+  }
+  toggleModal() {
+    this.setState({ modal: !this.state.modal });
+  }
+  renderRuns() {
     const rows = [];
     for (let i = 0; i < 8; i += 1) {
       rows.push(<Button
@@ -44,17 +53,20 @@ class BallScore extends Component {
       </Button>);
     }
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col style={{ textAlign: 'left' }} >
-              <h5> Runs: </h5>
-              {rows}
-            </Col>
-          </Row>
-        </Container>
-        <br />
-        <br />
+      <Container>
+        <Row>
+          <Col style={{ textAlign: 'left' }} >
+            <h5> Runs: </h5>
+            {rows}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+
+  renderExtras() {
+    return (
+      <Container>
         <Container>
           <Row>
             <Col style={{ textAlign: 'left' }} >
@@ -66,7 +78,7 @@ class BallScore extends Component {
           <Button
             color="info"
             onClick={event => this.setExtra(event)}
-            className="run-button"
+            className={`run-button ${this.state.extra === 'W' ? 'bg-blue' : ''}`}
             value="W"
           >
             W
@@ -74,7 +86,7 @@ class BallScore extends Component {
           <Button
             color="info"
             onClick={event => this.setExtra(event)}
-            className="run-button"
+            className={`run-button ${this.state.extra === 'N' ? 'bg-blue' : ''}`}
             value="N"
           >
             N
@@ -82,7 +94,7 @@ class BallScore extends Component {
           <Button
             color="info"
             onClick={event => this.setExtra(event)}
-            className="run-button"
+            className={`run-button ${this.state.extra === 'B' ? 'bg-blue' : ''}`}
             value="B"
           >
             B
@@ -90,38 +102,94 @@ class BallScore extends Component {
           <Button
             color="info"
             onClick={event => this.setExtra(event)}
-            className="run-button"
+            className={`run-button ${this.state.extra === 'LB' ? 'bg-blue' : ''}`}
             value="LB"
           >
             LB
           </Button>
         </Container>
+
+      </Container>
+    );
+  }
+
+  renderOut() {
+    return (
+      <Container>
+        <Col style={{ textAlign: 'center' }}>
+          <Button color="danger" onClick={() => this.toggleIsOut()}>
+            {'  '}
+            Out{'  '}
+          </Button>
+        </Col>
+      </Container>
+    );
+  }
+
+  renderNextBall() {
+    return (
+      <Container>
+        <Col style={{ textAlign: 'center' }}>
+          <Button
+            outline
+            color="primary"
+            onClick={() => this.onNextBallClick(this.state.run, this.state.extra)} //If out is pressed make the batsman empty and do all action for next ball click followed by modal display
+          >
+            Next Ball
+          </Button>
+        </Col>
+      </Container>
+    );
+  }
+
+  renderModalForNextBowler() {
+    if (this.state.isOut) {
+      return (
+        <div>
+          <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+            <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggleModal}>Do Something</Button>{' '}
+              <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+
+      );
+    }
+    return (
+      <div />
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.renderRuns()}
         <br />
-        <Container>
-          <Col style={{ textAlign: 'center' }}>
-            <Button color="danger">
-              {'  '}
-              Out{'  '}
-            </Button>
-          </Col>
-        </Container>
         <br />
-        <Container>
-          <Col style={{ textAlign: 'center' }}>
-            <Button
-              outline
-              color="primary"
-              onClick={() => this.onNextBallClick(this.state.run, this.state.extra)}
-            >
-              Next Ball
-            </Button>
-          </Col>
-        </Container>
+        {this.renderExtras()}
+        <br />
+        {this.renderOut()}
+        <br />
+        {this.renderNextBall()}
+        {this.renderModalForNextBowler()}
         {/* <button onClick={() => this.onNextBallClick(this.state.run)}>Next ball</button> */}
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  currentOverDetails: state.scoreBoardInformation.overDetails,
+  currentOver: state.scoreBoardInformation.team1.overNumber,
+  currentBall: state.scoreBoardInformation.team1.ballNumber,
+  currentBowler: state.scoreBoardInformation.currentBowler,
+});
+
 
 const mapDispatchProps = dispatch => ({
   handleCurrentBall: (run, extra) => {
