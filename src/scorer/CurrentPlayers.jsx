3d016c4fea38d7450e9de1804d27a3389b641f0e
sjthn <srijithn@thoughtworks.com>
2018-09-27 19:55:40 +0530

    toggle striker/non-striker names, reduce the view width

--- src/scorer/CurrentPlayers.jsx
+++ src/scorer/CurrentPlayers.jsx
@@ -1,39 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import './Scorer.css';
 
-let leftCellStyle = 'CurrentBatsman';
-let rightCellStyle = 'NonStrikeBatsman';
-
-class CurrentPlayers extends Component {
-  shouldComponentUpdate(nextProps) {
-    if (this.props.striker !== nextProps.striker) {
-      leftCellStyle = 'NonStrikeBatsman';
-      rightCellStyle = 'CurrentBatsman';
-    } else {
-      leftCellStyle = 'CurrentBatsman';
-      rightCellStyle = 'NonStrikeBatsman';
-    }
-    return (this.props.striker !== nextProps.striker && nextProps.striker);
-  }
-
-  render() {
-    return (
-      <div className="BatsmanDiv">
-        <div className={leftCellStyle}>
-          {leftCellStyle === 'CurrentBatsman' ? this.props.striker : this.props.nonStriker}
-        </div>
-        <div className={rightCellStyle}>
-          {rightCellStyle === 'CurrentBatsman' ? this.props.striker : this.props.nonStriker}
-        </div>
-      </div>
-    );
-  }
-}
+const CurrentPlayers = props => (
+  <div className="BatsmanDiv">
+    <div className="CurrentBatsman">
+      {!props.selectedBatsman ? props.striker : props.selectedBatsman}
+    </div>
+    <div className="NonStrikeBatsman">
+      {props.nonStriker}
+    </div>
+  </div>
+);
 
 const mapStateToProps = state => ({
   striker: state.scoreBoardInformation.striker,
   nonStriker: state.scoreBoardInformation.nonStriker,
+  selectedBatsman: state.scoreBoardInformation.selectedBatsman,
 });
 
 export default connect(mapStateToProps)(CurrentPlayers);
